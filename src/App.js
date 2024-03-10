918cf8db77fa43d4aad405147b0a0a49bf873f5a
Abhishek Gahlawat <abhishekgahlawatoo7@gmail.com>
2024-03-10 03:26:19 +0000

    Change react app to next app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Abhishek Gahlawat
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import image from './../public/logo512.png'
+ 
+export default function App() {
+  return <img src={image.src} />
 }
-
-export default App;
